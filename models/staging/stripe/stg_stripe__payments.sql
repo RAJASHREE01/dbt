12b5a42d20314be15d5fa3fbabef0ca3f377a744
Rajashree S <sekarrajashree@gmail.com>
2026-06-01 01:58:53 +0000

    added macro

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount / 100 as amount,
+    {{ cents_to_dollars(amount) }} as amount,
     created as created_at
 from {{source('jaffle_shop','payments')}}
